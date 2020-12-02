76fe00324ec3271e8d2225ed126464e424c00652
test <test@PRAGMA-PC43>
2020-12-02 18:21:22 +0530

    Add project files.

---
+++ Assignment1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {  
+        static void Main(string[] args)
+        {
+            List<String> Names = new List<String>{ "Gauri","Purva","Ekta","Aishwarya","Chaitali", "Anuja","Sayali","Neha"};       
+                //Console.WriteLine("Enter The Names");
+                //string result = Console.ReadLine();
+            //Console.WriteLine(Names.Contains(result));
+            //Console.ReadLine();
+
+            // string contains letter a
+            foreach (var item in Names)
+            {     
+                if (item.Contains("a") == true)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            //foreach (var item in Names)
+            //    {
+            //        if (item.Length % 2 != 0)
+            //        {
+            //            Console.WriteLine(item);
+            //        }
+            //    }
+        }
+    }
+}
